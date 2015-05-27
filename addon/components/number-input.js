8f41d1570f37f6665d448f1ea0739db00c4a6ea8
Leo Hernandez <lhernandez@thirdwave.it>
2015-05-26 21:00:04 -0400

    fixes

--- addon/components/number-input.js
+++ addon/components/number-input.js
@@ -1,33 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.TextField.extend({
-  getNewValue: function(e) {
-    let keyCode = e.keyCode || e.which;
-
-    return Ember.$(e.currentTarget).val() + String.fromCharCode(keyCode);
+  isValidKeyCode: function(keyCode) {
+    //46 = . char
+    //48...57 = 0...9 chars
+    return keyCode === 46  || (keyCode >= 48 && keyCode <= 57);
   },
 
-  validKeys: [
-    8, //backspace
-    9, //tab
-    38, //left arrow
-    39 //right arrow
-  ],
+  getNewValue: function(e) {
+    let keyCode = e.keyCode || e.which;
+    var newChar = this.isValidKeyCode(keyCode) ? String.fromCharCode(keyCode) : "";
 
-  isValidKey: function(keyCode) {
-    _.contains(this.validKeys, keyCode);
+    return Ember.$(e.currentTarget).val() + newChar;
   },
 
   moreThanTwoDecimals: function(e) {
     return this.getNewValue(e).countDecimals() > 2;
   },
 
+  isTab: function(keyCode) {
+    return keyCode === 9;
+  },
+
   isInvalidValue: function(e) {
-    let keyCode = e.keyCode || e.which;
-
-    if (this.isValidKey(keyCode)) {return true;}
-
     var value = this.getNewValue(e);
+
+    if (!value) {
+      return !this.isTab(e.keyCode);
+    }
+
     return isNaN(value) || value.countDecimals() > 2;
   },
 
